096ece76842164c296846e6259fffd35c4f6db99
임태민 <skaqndlrtm@naver.com>
2025-03-13 10:01:14 +0900

    FastAPI API 기능 추가

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "백엔드 서버 정상 작동 중!"}
+    return {"message": "서버 정상 작동 중!"}
+
+@app.post("/analyze-bodyfat")
+def analyze_bodyfat():
+    return {"message": "체지방률 분석 기능 미구현"}
