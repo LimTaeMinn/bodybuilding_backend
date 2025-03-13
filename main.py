06831b058a77f2219b8977a326aa73305d6a9ca9
임태민 <skaqndlrtm@naver.com>
2025-03-13 09:45:00 +0900

    main.py 수정됨

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()  # FastAPI 앱 객체 생성
+app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Backend!"}
+    return {"message": "백엔드 서버 정상 작동 중!"}
